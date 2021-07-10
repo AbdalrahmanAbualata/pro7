7c2850625dc4c4896019620f6e9c66032e064d91
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-07-10 18:37:12 +0300

    test6

--- app.js
+++ app.js
@@ -1,45 +1,50 @@
+'use strict';
+
+// ***********************************************************************************************************
+// let iconImgs = [];
+
+// function Img(name) {
+//   this.imgName = name.split(".")[0];
+//   this.filePath = name;
+//   iconImgs.push(this);
+// }
+
+// for (let i = 0; i < imgFile.length; i++) {
+//   // object maker
+//   new Img(imgFile[i]);
+// 
+// ************************************************************************************************************
 let imgContainer = document.getElementById("ob");
-
-let imgFile = ["ballon.png", "bekind.png", "pizza.png","ltcu.jpg",'grunge.png','Etcher.png'];
-let imgs = [];
-
-function Img(name) {
-  this.imgName = name.split(".")[0];
-  this.filePath = name;
-  imgs.push(this);
-}
-
-for (let i = 0; i < imgFile.length; i++) {
-  // object maker
-  new Img(imgFile[i]);
-}
-
+let imgFile = ["ballon.png", "bekind.png", "pizza.png", "ltcu.jpg", 'grunge.png', 'Etcher.png'];
 let imgEl;
 function render() {
-  for (let i = 0; i < imgs.length; i++) {
+  for (let i = 0; i < imgFile.length; i++) {
     let imgEl = document.createElement("img");
-    imgEl.setAttribute("src", "images/" + imgs[i].filePath);
+    imgEl.setAttribute("src", "images/" + imgFile[i]);
     imgEl.setAttribute("class", "item");
     imgEl.setAttribute("width", "100px");
     imgEl.setAttribute("id", i);
 
     imgContainer.appendChild(imgEl);
   }
-  
+
 }
 render();
 
 console.log(imgContainer);
-// let imgEl = document.querySelector(".item");
 imgContainer.addEventListener("click", goToCstmPage);
 
 function goToCstmPage(event) {
-console.log(event.target.id);
+  console.log(event.target.id);
   let imgIndex = event.target.id
-  let codedData = JSON.stringify(imgs[imgIndex].filePath);
-  localStorage.setItem("data", codedData);
+  let clickedicon= JSON.stringify(imgFile[imgIndex]);
+  localStorage.setItem("clickedicon", clickedicon);
+
+  let icons = JSON.stringify(imgFile);
+  localStorage.setItem("icons", icons);
   location.href = "shirt.html";
- 
+
 }
 
+// ********************************************************************************************************************
 
--- app2.js
+++ app2.js
@@ -1,43 +1,49 @@
+'use strict';
 let imgContainerEl = document.getElementById("closet");
 let changeColorContainer =document.getElementById('changeColor');
 let iconPlace = document.getElementById("shirrt");
-
+let iconsContenierEl=document.getElementById("icons")
+let icons ;
+let  iconImg;
+let  newIcon;
 function placeIcon() {
-  const iconImg = JSON.parse(localStorage.getItem('data')) || [];
-
-    let newIcon = document.createElement("img");
+  console.log(iconImg);
+ iconImg = JSON.parse(localStorage.getItem('clickedicon')) || [];
+ icons = JSON.parse(localStorage.getItem('icons')) || [];
+     newIcon = document.createElement("img");
     newIcon.setAttribute("src", "images/" + iconImg);
     newIcon.setAttribute("class", "icon");
 
     imgContainerEl.appendChild(newIcon);
   }
-
+  
 placeIcon();
 
 let imgs=[];
 let tsColor=[];
 for (let i = 2; i < 15; i++) {
-  imgs.push(`${i}.png`);
+  imgs.push(`${i}a.jpg`);
   tsColor.push(`${i}.png`);
 }
 console.log(tsColor);
-let imgEl;
+let imgEl2;
 
-function render() {
-  for (let i = 0; i < imgs.length; i++) {
+function render(lengTh,imgFile,contenerName,changeClass) {
+  for (let i = 0; i < lengTh; i++) {
     // liEl = document.createElement("li");
     imgEl2 = document.createElement("img");
-    imgEl2.setAttribute("src", "images/" + imgs[i]);
-    imgEl2.setAttribute("class", "circle");
+    imgEl2.setAttribute("src", "images/" + imgFile[i]);
+    imgEl2.setAttribute("class",changeClass );
     imgEl2.setAttribute("width", "40px");
     imgEl2.setAttribute("id", i);
     // liEl.appendChild(imgEl);
-    changeColorContainer.appendChild(imgEl2);
+    contenerName.appendChild(imgEl2);
     
   }
 }
-render();
-
+console.log(icons);
+render(imgs.length,imgs,changeColorContainer,'circle');
+render(icons.length,icons,iconsContenierEl,'icons');
 
 
 let  imgTsEl;
@@ -51,5 +57,55 @@ function changeColor (event){
   imgTsEl.setAttribute('class','shirrt')
   imgTsEl.setAttribute("src", "images/"+tsColor[ColorIndex]);
   imgContainerEl.appendChild(imgTsEl);
+
+  let tsColorInLlocal = JSON.stringify(tsColor[ColorIndex]);
+  localStorage.setItem("tsColor", tsColorInLlocal);
+
   placeIcon();
 }
+
+iconsContenierEl.addEventListener("click",  changIcon);
+
+function changIcon(event){
+  let clickedicon= JSON.stringify(icons[event.target.id]);
+  localStorage.setItem("clickedicon", clickedicon);
+  newIcon.remove();
+  placeIcon();
+}
+
+// *****************************************************************************************
+
+const Cart = function(items) {
+  this.items = items;
+};
+
+Cart.prototype.addItem = function(product, quantity) {
+
+  const newCartItem = new CartItem(product, quantity);
+  cart.items.push(newCartItem );
+
+};
+
+
+Cart.prototype.saveToLocalStorage = function() {
+  
+
+  let cartContent = JSON.stringify(cart.items);
+  localStorage.setItem('cart',cartContent);
+};
+
+Cart.prototype.removeItem = function(item) {
+  
+  this.items.splice(item,1);
+
+
+
+
+};
+
+const CartItem = function(product, quantity) {
+  this.product = product;
+  this.quantity = quantity;
+};
+
+
