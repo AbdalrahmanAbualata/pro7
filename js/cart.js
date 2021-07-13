801b5be7f6bbeb1a557d5a532788176382455de5
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-07-13 15:37:42 +0300

    sss

--- js/cart.js
+++ js/cart.js
@@ -3,7 +3,7 @@
 let table = document.getElementById("cart");
 let tbodyEl = document.getElementById("body");
 table.appendChild(tbodyEl);
-table.addEventListener('click', removeItemFromCart);
+
 
 let tshirt = {
   design: [],
@@ -40,6 +40,7 @@ function showCart() {
     tdEl = document.createElement("td");
     aEl = document.createElement("a");
     aEl.innerHTML = `<i id='${i}' class="far fa-trash-alt"></i>`;
+    tdEl.addEventListener('click', removeItemFromCart);
     tdEl.appendChild(aEl);
     trEl.appendChild(tdEl);
     let tdEl1 = document.createElement("td");
@@ -52,18 +53,28 @@ function showCart() {
     let tdEl4 = document.createElement("td");
     let img = document.createElement("img");
     img.setAttribute("src", "images/icon/" + tshirt.design[i].icon);
-    img.setAttribute("width", "40px");
-    img.setAttribute("height", "40px");
+    img.setAttribute("width", "50px");
+    img.setAttribute("height", "50px");
     tdEl4.appendChild(img);
-    // *******************************************************************
     trEl.appendChild(tdEl4);
+    // *******************************************************************
+    let tdEl5 = document.createElement("td");
+    let img2 = document.createElement("img");
+    img2.setAttribute("src", "images/tshirt/"+tshirt.design[i].tsColor);
+    img2.setAttribute("width", "60px");
+    img2.setAttribute("height", "60px");
+    tdEl5.appendChild(img2);
+    trEl.appendChild(tdEl5);
+    // ********************************************************************
     let tdEl3 = document.createElement("td");
     tdEl3.textContent = `${tshirt.design[i].tsColor.split(".")[0]}`;
     trEl.appendChild(tdEl3);
     tbodyEl.appendChild(trEl);
   }
-
 }
+
+
+
 
 
 
